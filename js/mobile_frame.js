eb3bba1eec8078387bdbc945046d08c4bd40577b
Katya Fadeeva <katfadmail@gmail.com>
2018-10-31 20:35:31 +0200

    text in canvas

---
+++ js/mobile_frame.js
@@ -0,0 +1,8 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+ctx.font = "500 36px Ubuntu";
+ctx.fillStyle = "#989587";
+ctx.textAlign = "left";
+ctx.fillText("Canvas", 20, 50);
+ctx.fillText("360x640", 20, 100);
+ctx.fillText("Portrait", 20, 150);
